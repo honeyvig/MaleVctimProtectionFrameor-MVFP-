b8307fb6d06cc3a66f919a005b3035b30ef1c89c
honeyvig <honeyvig@gmail.com>
2025-12-15 13:10:01 +0530

    Create ipc_rights.py

---
+++ legal_layer/ipc_rights.py
@@ -0,0 +1,9 @@
+IPC_RIGHTS = {
+    "Poisoning": "IPC 328 – Causing hurt by means of poison",
+    "Criminal intimidation": "IPC 506",
+    "Assault": "IPC 351",
+    "Extortion": "IPC 383",
+}
+
+def explain_rights():
+    return IPC_RIGHTS
